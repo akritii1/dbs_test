dca9fe9393870faffe3db32f29d8a0011f88efc0
zerocabbages <72064889+zerocabbages@users.noreply.github.com>
2020-10-06 19:29:16 +0800

    Add files via upload

---
+++ Maniteja_TS/votingStatus.js
@@ -0,0 +1,3 @@
+var age = 19;
+var result = age >= 18 ? "Eligible fro voting " : "Not eligible for voting";
+console.log(result);
---
+++ Maniteja_TS/votingStatus.ts
@@ -0,0 +1,3 @@
+var age = 19;
+var result  = age >= 18 ? "Eligible for voting " : "Not eligible for voting";
+console.log(result)
