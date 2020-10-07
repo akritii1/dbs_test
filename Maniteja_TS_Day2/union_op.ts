5faf14fb18e4516eb88d901deb6e1c07ac3365a4
zerocabbages <72064889+zerocabbages@users.noreply.github.com>
2020-10-07 18:30:12 +0800

    Add files via upload

---
+++ Maniteja_TS_Day2/union_op.ts
@@ -0,0 +1,20 @@
+var data: number|string;
+
+function palindrome(data){
+	return data == data.split('').reverse().join('');
+}
+
+function palindrome1(data){
+	var len=data.length;
+	for (var i=0;i<len/2; i++){
+		if(data[i] !== data[len - 1 -i]){
+			console.log("this is not a palindorme");
+		}
+		console.log("this is a palindrome");
+	}
+	
+	
+}
+
+palindrome1(15);
+palindrome1("eye");
