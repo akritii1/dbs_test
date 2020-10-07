9fe98c1f91f006ee7e9310e126820f22705afcdc
SaiRoja98 <72065085+SaiRoja98@users.noreply.github.com>
2020-10-07 19:53:33 +0800

    Create bank.js

---
+++ SaiRoja_TS_Day-2/bank.js
@@ -0,0 +1,18 @@
+var Bank = /** @class */ (function () {
+    function Bank() {
+    }
+    Bank.prototype.assign_data = function (acc_no, name, balance) {
+        this.acc_no = acc_no;
+        this.name = name;
+        this.balance = balance;
+    };
+    Bank.prototype.display = function () {
+        console.log("Account_number : " + this.acc_no);
+        console.log("Name : " + this.name);
+        console.log("Balance : " + this.balance);
+    };
+    return Bank;
+}());
+var bank_obj = new Bank();
+bank_obj.assign_data(123, "Ram", 5000000);
+bank_obj.display();
